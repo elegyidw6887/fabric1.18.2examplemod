8c4de2126f46eda1733b70842516a5be5590b47c
一曲哀歌 <654725797@qq.com>
2022-08-02 21:03:20 +0800

    push 1.新增维度传送门（使用第三方API实现）

--- src/main/java/examplefabricmod/world/dimension/ModDimensions.java
+++ src/main/java/examplefabricmod/world/dimension/ModDimensions.java
@@ -1,6 +1,9 @@
 package examplefabricmod.world.dimension;
 
 import examplefabricmod.ExampleFabricMod;
+import examplefabricmod.block.ModBlocks;
+import examplefabricmod.item.ModItems;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -15,6 +18,14 @@ public class ModDimensions {
             EXAMPLE_DIMENSION_KEY.getValue());
 
     public static void registerModDimensions() {
+
+        CustomPortalBuilder.beginPortal()
+                .frameBlock(ModBlocks.AMETHYST_BLOCK)
+                .destDimID(EXAMPLE_DIMENSION_KEY.getValue())
+                .tintColor(45, 79, 135)
+                .lightWithItem(ModItems.AMETHYST)
+                .onlyLightInOverworld()
+                .registerPortal();
 
         ExampleFabricMod.LOGGER.info("Registering ModDimensions for " + ExampleFabricMod.MOD_ID);
     }
